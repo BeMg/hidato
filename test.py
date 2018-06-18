1fb9dda98c5fbb27ec1468bbebd9170b356ad478
bemg <gccbg04538@gmail.com>
2018-06-18 12:32:04 +0800

    update test

--- test.py
+++ test.py
@@ -4,28 +4,33 @@ import json
 
 if __name__=='__main__':
     # hidato size
-    n = 25
-    group_size = 10000
-    round = 1000
-    record = []
-    for i in [x+5 for x in range(n-5+1)]: # size
-        for j in [50+50*x for x in range(20)]: # round
-            for k in [0.2*x for x in range(11)]: # alive rate
-                for l in [0.2*x for x in range(11)]: # mutation rate
-                    for m in [1000+100*x for x in range(20)]: # group size
-                        cnt = 0
-                        best = random_hidato_generator(i, i)
-                        while True and cnt < 100:
-                            cnt += 1
-                            tmp = random_hidato_generator(i, i)
-                            if tmp.n > best.n:
-                                best = tmp
-                        g = GA()
-                        g.set_alive_rate(float(k))
-                        g.set_group_size(int(m))
-                        g.set_mutation_rate(float(l))
-                        g.set_round(int(j))
-                        rst = g.run(best)
-                        record.append(rst)
-                        with open('data.json', 'w') as f:
-                            json.dump(record, f, ensure_ascii=False)
+
+    # 5*5 -> 50*50 with step equal to 5
+    for i in range(10):
+        n = (i+1)*5
+        group_size = 10000
+        muation_rate = 0.6
+        alive_rate = 0.4
+        round = 30
+
+        cnt = 0
+        best = random_hidato_generator(n, n)
+        while True and cnt < n*50:
+            cnt += 1
+            tmp = random_hidato_generator(n, n)
+            if tmp.n > best.n:
+                best = tmp
+
+        # every setting run 20 time get the average
+        result = []
+        for j in range(20):
+            g = GA()
+            g.set_round(round)
+            g.set_mutation_rate(muation_rate)
+            g.set_group_size(group_size)
+            g.set_alive_rate(alive_rate)
+            rst = g.run(best)
+            result.append(rst)
+
+        with open('hidato_{}.json'.format(n), 'w') as f:
+            json.dump(result, f, ensure_ascii=False)
